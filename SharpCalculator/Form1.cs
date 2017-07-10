e49e29a4e17732769d9d3febc9d2a87a6aa3f61b
Alexandr Pelipenko <hard.b13f@gmail.com>
2017-07-10 15:18:36 +0600

    Change logics

--- SharpCalculator/Form1.cs
+++ SharpCalculator/Form1.cs
@@ -12,6 +12,8 @@ namespace SharpCalculator
 {
     public partial class Form1 : Form
     {
+
+        double arg1, arg2, answer;
         public Form1()
         {
             InitializeComponent();
@@ -24,22 +26,30 @@ namespace SharpCalculator
 
         private void buttonPlus_Click(object sender, EventArgs e)
         {
-            textBoxOutput.Text = (Convert.ToDouble(textBoxInput1.Text.ToString()) + Convert.ToDouble(textBoxInput2.Text.ToString())).ToString();
+            arg1 = Convert.ToDouble(textBoxInput1.Text.ToString());
+            arg2 = Convert.ToDouble(textBoxInput2.Text.ToString());
+            textBoxOutput.Text = (arg1 + arg2).ToString();
         }
 
         private void buttonMinus_Click(object sender, EventArgs e)
         {
-            textBoxOutput.Text = (Convert.ToDouble(textBoxInput1.Text.ToString()) - Convert.ToDouble(textBoxInput2.Text.ToString())).ToString();
+            arg1 = Convert.ToDouble(textBoxInput1.Text.ToString());
+            arg2 = Convert.ToDouble(textBoxInput2.Text.ToString());
+            textBoxOutput.Text = (arg1 - arg2).ToString();
         }
 
         private void buttonMultiply_Click(object sender, EventArgs e)
         {
-            textBoxOutput.Text = (Convert.ToDouble(textBoxInput1.Text.ToString()) * Convert.ToDouble(textBoxInput2.Text.ToString())).ToString();
+            arg1 = Convert.ToDouble(textBoxInput1.Text.ToString());
+            arg2 = Convert.ToDouble(textBoxInput2.Text.ToString());
+            textBoxOutput.Text = (arg1 * arg2).ToString();
         }
 
         private void buttonDivide_Click(object sender, EventArgs e)
         {
-            textBoxOutput.Text = (Convert.ToDouble(textBoxInput1.Text.ToString()) / Convert.ToDouble(textBoxInput2.Text.ToString())).ToString();
+            arg1 = Convert.ToDouble(textBoxInput1.Text.ToString());
+            arg2 = Convert.ToDouble(textBoxInput2.Text.ToString());
+            textBoxOutput.Text = (arg1 / arg2).ToString();
         }
     }
 }
